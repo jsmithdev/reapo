54029a412307d4388d8778c27fe4ee5dd8ad5072
Jamie Smith <i@jamiesmiths.com>
2019-03-30 21:25:45 -0400

    🚀 dep update, forge v6, 0 vulns = #4 done

--- forge.config.js
+++ forge.config.js
@@ -1,38 +1,36 @@
 // jshint esversion:6, asi: true, laxcomma: true
 
-const path = require('path');
+const path = require('path')
+const icon = path.resolve(__dirname, 'resources/icon.png')
 
 module.exports = {
-    "forge": {
-        "make_targets": {
-          "win32": [
-            "squirrel"
-          ],
-          "darwin": [
-            "zip"
-          ],
-          "linux": [
-            "deb",
-            "rpm"
-          ]
-        },
-        "electronPackagerConfig": {
-          "packageManager": "npm",
-          "icon": path.resolve(__dirname, '/src/icon.png')
-        },
-        "electronWinstallerConfig": {
-          "name": "reapo"
-        },
-        "electronInstallerDebian": {},
-        "electronInstallerRedhat": {},
-        "github_repository": {
-          "owner": "jsmithdev",
-          "name": "reapo",
-          "prerelease": true
-        },
-        "windowsStoreConfig": {
-          "packageName": "reapo",
-          "name": "reapo"
-        }
+
+  "packagerConfig": {
+    icon,
+    "packageManager": "npm", 
+  },
+  "github_repository": {
+    "owner": "jsmithdev",
+    "name": "reapo",
+    "prerelease": true
+  },
+  makers: [
+    {
+      "name": "@electron-forge/maker-squirrel",
+    },
+    {
+      "name": "@electron-forge/maker-zip",
+      "platforms": [
+        "darwin"
+      ]
+    },
+    {
+      "name": "@electron-forge/maker-deb",
+      "config": { }
+    },
+    {
+      "name": "@electron-forge/maker-rpm",
+      "config": {}
     }
+  ]
 }
