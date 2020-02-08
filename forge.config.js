e509a8f2e2b309df386c71b6733f9d2ac5295fea
jsmithdev <me@jsmith.dev>
2020-02-07 21:42:08 -0500

    rmv token&share; lessen friction

---
+++ forge.config.js
@@ -0,0 +1,71 @@
+
+const path = require('path')
+const icon = path.resolve(`${__dirname}/assets/icons/png/icon.icns`)
+
+module.exports = {
+
+	packagerConfig: {
+		icon,
+		license: 'MIT',
+		packageManager: 'npm', 
+	},
+	github_repository: {
+		owner: 'jsmithdev',
+		name: 'reapo',
+		prerelease: true
+	},
+	makers: [{
+		name: '@electron-forge/maker-squirrel',
+	},
+	{
+		name: '@electron-forge/maker-zip',
+		platforms: [
+			'darwin'
+		]
+	},
+	{
+		name: '@electron-forge/maker-deb',
+		config: {
+			productName: "Reapo",
+			license: "MIT",
+			maintainer: "Jamie Smith",
+			homepage: "https://github.com/jsmithdev/reapo",
+			icon: "resources/icon.png",
+			categories: ['Development']
+		}
+	}],
+	publishers: {
+		name: '@electron-forge/publisher-github',
+		platforms: ['deb'],
+		config: {
+		  repository: {
+			owner: 'jsmithdev',
+			name: 'reapo'
+		  },
+		  authToken: '',
+		  prerelease: true
+		}
+	}
+}
+
+
+/* 
+,
+	{
+		name: '@electron-forge/maker-rpm',
+		config: {}
+	}
+
+	
+	{
+		name: '@electron-forge/maker-snap',
+		config: {
+			features: {
+			audio: false,
+			mpris: 'com.reapo.mpris',
+			webgl: false
+			},
+			summary: 'Repo manager built with web standards'
+	}
+},
+*/
