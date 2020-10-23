446cf04c8539e9084b130ac27a65a470c4a152bd
Jamie Smith <me@jsmith.dev>
2020-10-23 04:18:19 -0400

    continue building actionable issue counter

--- src/components/github-issues-count/github-issues-count.js
+++ src/components/github-issues-count/github-issues-count.js
@@ -12,12 +12,23 @@ template.innerHTML = /*html*/`
     vertical-align: middle;
     fill: var(--color-light);
 }
+.hidden {
+    display: none;
+}
 </style>
 
 
-<svg class="icon_small no_data" viewBox="0 0 24 24">
-    <path d="M19 12V13.5C21.21 13.5 23 15.29 23 17.5C23 18.32 22.75 19.08 22.33 19.71L21.24 18.62C21.41 18.28 21.5 17.9 21.5 17.5C21.5 16.12 20.38 15 19 15V16.5L16.75 14.25L16.72 14.22C16.78 14.17 16.85 14.13 19 12M19 23V21.5C16.79 21.5 15 19.71 15 17.5C15 16.68 15.25 15.92 15.67 15.29L16.76 16.38C16.59 16.72 16.5 17.1 16.5 17.5C16.5 18.88 17.62 20 19 20V18.5L21.25 20.75L21.28 20.78C21.22 20.83 21.15 20.87 19 23M13.03 18H6C3.79 18 2 16.21 2 14S3.79 10 6 10H6.71C7.37 7.69 9.5 6 12 6C15 6 17.4 8.37 17.5 11.32C18.12 11.11 18.8 11 19.5 11C20.78 11 21.97 11.38 23 12C22.13 10.9 20.84 10.14 19.35 10.03C18.67 6.59 15.64 4 12 4C9.11 4 6.6 5.64 5.35 8.03C2.34 8.36 0 10.9 0 14C0 17.31 2.69 20 6 20H13.5C13.24 19.38 13.08 18.7 13.03 18Z" />
-</svg>
+<div class="no_git" class="hidden" title="Project is not a git repo">
+    <svg class="icon_small" title="Not a git repo" viewBox="0 0 24 24">
+        <path d="M4,1C2.89,1 2,1.89 2,3V7C2,8.11 2.89,9 4,9H1V11H13V9H10C11.11,9 12,8.11 12,7V3C12,1.89 11.11,1 10,1H4M4,3H10V7H4V3M14,13C12.89,13 12,13.89 12,15V19C12,20.11 12.89,21 14,21H11V23H23V21H20C21.11,21 22,20.11 22,19V15C22,13.89 21.11,13 20,13H14M3.88,13.46L2.46,14.88L4.59,17L2.46,19.12L3.88,20.54L6,18.41L8.12,20.54L9.54,19.12L7.41,17L9.54,14.88L8.12,13.46L6,15.59L3.88,13.46M14,15H20V19H14V15Z" />
+    </svg>
+</div>
+
+<div class="no_data" class="hidden" title="Project is a git repo, click to check issues">
+    <svg class="icon_small " title="Click to get issues" viewBox="0 0 24 24">
+        <path  d="M3,5H9V11H3V5M5,7V9H7V7H5M11,7H21V9H11V7M11,15H21V17H11V15M5,20L1.5,16.5L2.91,15.09L5,17.17L9.59,12.59L11,14L5,20Z" />
+    </svg>
+</div>
 
 `
 
@@ -25,7 +36,6 @@ export class GithubIssuesCount extends HTMLElement {
 
     constructor() {
         super()
-        //console.log('hi from constructor')
         this.attachShadow({mode: 'open'})
     }
 	
@@ -34,7 +44,7 @@ export class GithubIssuesCount extends HTMLElement {
     }
 
     static get observedAttributes() {
-        return ['repo']
+        return ['git']
     }
 
     connectedCallback() {
@@ -44,16 +54,32 @@ export class GithubIssuesCount extends HTMLElement {
     }
 	
     registerElements(doc){
-        //console.log('registerElements')
         
         this.dom = {
+            no_git: doc.querySelector('.no_git'),
             no_data: doc.querySelector('.no_data'),
         }
-	    
+        
+        this.setup()
+    }
+
+    setup(){
+        
+        if(this.git){
+            this.dom.no_git.classList.add('hidden')
+            this.dom.no_data.classList.remove('hidden')
+        }
+        else {
+            this.dom.no_git.classList.remove('hidden')
+            this.dom.no_data.classList.add('hidden')
+        }
+
 		this.registerListeners()
     }
 	
 	registerListeners(){
+
+        this.dom.no_git.onclick = () => this.toast('Project is not a git repo...')
 	
         this.dom.no_data.onclick = () => {
 
@@ -71,20 +97,27 @@ export class GithubIssuesCount extends HTMLElement {
             console.log('todo')
             console.log(todo)
 
-
+            // todo 
+            this.toast('In development: Project is a git repo! Issues are next todo :unicorn:')
         }
 	}
-	
+
+	toast(msg, res){
+		this.dispatchEvent(
+			new CustomEvent(
+				'toast',
+				{
+					bubbles: true,
+					composed: true,
+					detail: { msg, res }
+				}
+			)
+		)
+    }
+    
     attributeChangedCallback(n, ov, nv) {
-        super.attributeChangedCallback(n, ov, nv);
-        console.dir(n)
-        console.dir(ov)
-        console.dir(nv)
-        //switch (n) {
-        //    case 'attr name that changed!':
-        //        ov !== nv // old val not equal new val
-        //        break;
-        //}
+        
+        this[n] = nv
     }
 }
 
