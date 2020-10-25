56d793879e0dee0d84bf40db7dfb90c95f4640f5
Jamie Smith <me@jsmith.dev>
2020-10-25 07:21:01 -0400

    imp issues, modal, button, wrap up

--- src/components/github-issues-count/github-issues-count.js
+++ src/components/github-issues-count/github-issues-count.js
@@ -4,34 +4,6 @@
 */
 'use strict()'
 
-const template = document.createElement('template')
-template.innerHTML = /*html*/`
-<style>
-.icon_small {
-    margin: 2px;
-    vertical-align: middle;
-    fill: var(--color-light);
-}
-.hidden {
-    display: none;
-}
-</style>
-
-
-<div class="no_git" class="hidden" title="Project is not a git repo">
-    <svg class="icon_small" title="Not a git repo" viewBox="0 0 24 24">
-        <path d="M4,1C2.89,1 2,1.89 2,3V7C2,8.11 2.89,9 4,9H1V11H13V9H10C11.11,9 12,8.11 12,7V3C12,1.89 11.11,1 10,1H4M4,3H10V7H4V3M14,13C12.89,13 12,13.89 12,15V19C12,20.11 12.89,21 14,21H11V23H23V21H20C21.11,21 22,20.11 22,19V15C22,13.89 21.11,13 20,13H14M3.88,13.46L2.46,14.88L4.59,17L2.46,19.12L3.88,20.54L6,18.41L8.12,20.54L9.54,19.12L7.41,17L9.54,14.88L8.12,13.46L6,15.59L3.88,13.46M14,15H20V19H14V15Z" />
-    </svg>
-</div>
-
-<div class="no_data" class="hidden" title="Project is a git repo, click to check issues">
-    <svg class="icon_small " title="Click to get issues" viewBox="0 0 24 24">
-        <path  d="M3,5H9V11H3V5M5,7V9H7V7H5M11,7H21V9H11V7M11,15H21V17H11V15M5,20L1.5,16.5L2.91,15.09L5,17.17L9.59,12.59L11,14L5,20Z" />
-    </svg>
-</div>
-
-`
-
 export class GithubIssuesCount extends HTMLElement {
 
     constructor() {
@@ -44,11 +16,46 @@ export class GithubIssuesCount extends HTMLElement {
     }
 
     static get observedAttributes() {
-        return ['git']
+        return ['git', 'repo', 'issues']
+    }
+
+    get count(){
+        return this._issues.length
+    }
+
+    get template(){
+        return this.getTemplate()
+    }
+
+    get issues(){ return this._issues }
+    set issues(value){
+        
+        if(!this._issues){ 
+
+            this._issues = JSON.parse( value )
+            
+            return this.setupHasIssues() 
+        }
+
+        this._issues = JSON.parse( value )
+    }
+    get issueRows(){
+
+        const rows = this.issues?.map(issue => {
+            console.log(issue)
+            return `
+                <h2><a href="${issue.url}">${issue.title}</a></h2>
+                <pre title="${issue.date}">${issue.body}</pre>
+                <br/>
+            `
+        }).join('')
+        
+        return rows ? rows : ''
     }
 
     connectedCallback() {
-        this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+        this.shadowRoot.appendChild(this.template.content.cloneNode(true))
         
         this.registerElements(this.shadowRoot)
     }
@@ -83,25 +90,17 @@ export class GithubIssuesCount extends HTMLElement {
 	
         this.dom.no_data.onclick = () => {
 
-            const todo = `
-            - if clicked and no data
-                - get repo
-                - get username
-                - get auth token
-                - get issues
-                - save localStorage
-                - show number of issues as icon
-            - if clicked and has data
-                - open modal with issues listed
-            `
-            console.log('todo')
-            console.log(todo)
-
-            // todo 
-            this.toast('In development: Project is a git repo! Issues are next todo :unicorn:')
+            this.dispatchEvent(new CustomEvent('get-issues-count', {
+                detail: {
+					bubbles: true,
+					composed: true,
+                    repo: this.repo,
+                }
+            }))
         }
-	}
-
+    }
+    
+    //  this.toast('todo :unicorn:')
 	toast(msg, res){
 		this.dispatchEvent(
 			new CustomEvent(
@@ -118,6 +117,118 @@ export class GithubIssuesCount extends HTMLElement {
     attributeChangedCallback(n, ov, nv) {
         
         this[n] = nv
+    }
+
+
+
+    openLink(url){
+        console.log('url =>')
+        console.log(url)
+    }
+
+    addModal(){
+        const div = document.createElement( 'div' )
+        div.innerHTML = /* html */`
+        <modal-component
+            header="default"
+            body-align="left"
+            body-color="${document.documentElement.style.getPropertyValue('--color-lightest')}"
+            body-border="${document.documentElement.style.getPropertyValue('--color-dark')}"
+            body-background="${document.documentElement.style.getPropertyValue('--color-dark')}"
+            footer-separator="${document.documentElement.style.getPropertyValue('--color-light')}"
+            close-icon-color="${document.documentElement.style.getPropertyValue('--color-lightest')}">
+        
+            <span slot="trigger">
+                <div class="has_data hidden"></div>
+            </span>
+        
+            <span slot="header">
+                ${this.repo?.substring(this.repo.lastIndexOf('/')+1, this.repo.length)} Issues
+            </span>
+        
+            <span slot="content">
+                ${this.issueRows}
+            </span>
+        
+            <span slot="footer">
+                <reapo-button
+                    name="close"
+                    label="Close"
+                    color="${document.documentElement.style.getPropertyValue('--color-lightest')}"
+                    background="${document.documentElement.style.getPropertyValue('--color-dark')}"
+                ></reapo-button>
+            </span>
+    
+        </modal-component>
+        `
+
+        this.shadowRoot.appendChild( div )
+
+        
+    }
+
+    setupHasIssues(){
+        
+        this.addModal()
+
+        setTimeout(() => {
+
+            this.dom.has_data = this.shadowRoot.querySelector('.has_data')
+
+            const names = this._issues.map(x => x.title).join('\n')
+
+            this.dom.has_data.textContent = this.count
+            this.dom.has_data.title = `Repo has ${this.count} open issues:\n${names}`
+
+            this.dom.no_data.classList.add('hidden')
+            this.dom.has_data.classList.remove('hidden')
+            this.shadowRoot.querySelector('modal-component').querySelector('reapo-button')
+                .onclick = _ => this.shadowRoot.querySelector('modal-component').close()
+        }, 0)
+    }
+
+    getTemplate(){
+
+        const template = document.createElement('template')
+
+        template.innerHTML = /*html*/`
+        
+        <style>
+            h2>a {
+                color: var(--color-light)
+            }
+            .icon_small {
+                margin: 2px;
+                vertical-align: middle;
+                fill: var(--color-light);
+            }
+            .hidden {
+                display: none;
+            }
+            .has_data {
+                border-radius: 50%;
+                height: 2rem;
+                width: 2rem;
+                line-height: 2rem;
+                color: var(--color-light);
+                background: var(--color-dark);
+            }
+        </style>
+        
+        <div class="no_git" class="hidden" title="Project is not a git repo">
+            <svg class="icon_small" title="Not a git repo" viewBox="0 0 24 24">
+                <path d="M4,1C2.89,1 2,1.89 2,3V7C2,8.11 2.89,9 4,9H1V11H13V9H10C11.11,9 12,8.11 12,7V3C12,1.89 11.11,1 10,1H4M4,3H10V7H4V3M14,13C12.89,13 12,13.89 12,15V19C12,20.11 12.89,21 14,21H11V23H23V21H20C21.11,21 22,20.11 22,19V15C22,13.89 21.11,13 20,13H14M3.88,13.46L2.46,14.88L4.59,17L2.46,19.12L3.88,20.54L6,18.41L8.12,20.54L9.54,19.12L7.41,17L9.54,14.88L8.12,13.46L6,15.59L3.88,13.46M14,15H20V19H14V15Z" />
+            </svg>
+        </div>
+        
+        <div class="no_data" class="hidden" title="Project is a git repo, click to check issues">
+            <svg class="icon_small " title="Click to get issues" viewBox="0 0 24 24">
+                <path  d="M3,5H9V11H3V5M5,7V9H7V7H5M11,7H21V9H11V7M11,15H21V17H11V15M5,20L1.5,16.5L2.91,15.09L5,17.17L9.59,12.59L11,14L5,20Z" />
+            </svg>
+        </div>
+        `
+
+        return template
     }
 }
 
